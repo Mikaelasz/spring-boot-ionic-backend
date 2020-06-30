c7885a3537c8dbb999e0a14148c7fef5ee5abfd8
Mikaela Soares <miih.arj@gmail.com>
2020-06-30 16:09:04 -0300

    Validação sintatica com Bean Validation

--- src/main/java/com/mikaelasoares/cursomc/services/CategoriaService.java
+++ src/main/java/com/mikaelasoares/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.mikaelasoares.cursomc.domain.Categoria;
+import com.mikaelasoares.cursomc.dto.CategoriaDTO;
 import com.mikaelasoares.cursomc.repositories.CategoriaRepository;
 import com.mikaelasoares.cursomc.services.exceptions.DataIntegrityException;
 import com.mikaelasoares.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,6 +57,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
 				orderBy);
 		return repo.findAll(pageRequest);
+	}
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }

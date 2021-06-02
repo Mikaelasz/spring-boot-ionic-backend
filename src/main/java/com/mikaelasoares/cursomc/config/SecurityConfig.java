0aeeed08e9271ffe1922fe645c83038fa2f01540
Mikaela Soares <miih.arj@gmail.com>
2021-06-02 17:20:15 -0300

    Implementando autorizacao

--- src/main/java/com/mikaelasoares/cursomc/config/SecurityConfig.java
+++ src/main/java/com/mikaelasoares/cursomc/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.mikaelasoares.cursomc.config;
 
 import com.mikaelasoares.cursomc.security.JWTAuthenticationFilter;
+import com.mikaelasoares.cursomc.security.JWTAuthorizationFilter;
 import com.mikaelasoares.cursomc.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -54,6 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 

87d792159633cb747763f259c01faa58905510fc
Mikaela Soares <miih.arj@gmail.com>
2021-06-03 12:34:52 -0300

    Refesh token

---
+++ src/main/java/com/mikaelasoares/cursomc/resources/AuthResource.java
@@ -0,0 +1,30 @@
+package com.mikaelasoares.cursomc.resources;
+
+import com.mikaelasoares.cursomc.security.JWTUtil;
+import com.mikaelasoares.cursomc.security.UserSS;
+import com.mikaelasoares.cursomc.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    @RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+        UserSS user = UserService.authenticated();
+        String token = jwtUtil.generateToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+    }
+}

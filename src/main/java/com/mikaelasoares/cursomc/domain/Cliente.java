7d94355b58b40517594b05496e09ea7913d915da
Mikaela Soares <miih.arj@gmail.com>
2021-08-16 18:22:28 -0300

    Salvando a URL da imagem em cliente

--- src/main/java/com/mikaelasoares/cursomc/domain/Cliente.java
+++ src/main/java/com/mikaelasoares/cursomc/domain/Cliente.java
@@ -43,7 +43,11 @@ public class Cliente implements Serializable {
 	
 	@JsonIgnore
 	@OneToMany(mappedBy="cliente")
-	private List<Pedido> pedidos = new ArrayList<>(); 
+	private List<Pedido> pedidos = new ArrayList<>();
+
+
+
+	private String imageUrl;
 	
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
@@ -164,5 +168,14 @@ public class Cliente implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
 	}
 }

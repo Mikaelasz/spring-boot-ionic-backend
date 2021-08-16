2ee26f5e3bc618f3000a6e7758affa46a9b59d11
Mikaela Soares <miih.arj@gmail.com>
2021-08-16 16:59:42 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/mikaelasoares/cursomc/CursomcApplication.java
+++ src/main/java/com/mikaelasoares/cursomc/CursomcApplication.java
@@ -1,18 +1,27 @@
 package com.mikaelasoares.cursomc;
 
+import com.mikaelasoares.cursomc.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
-	
+
+	@Autowired
+	private S3Service s3Service;
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+
+
 	@Override
-	public void run(String... args) throws Exception {		
+	public void run(String... args) throws Exception {
+		s3Service.uploadFile("D:\\Mikaela\\como-comecar-a-programar-1.JPG");
 		
 	}
 }
